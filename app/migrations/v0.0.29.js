6f02be449d94b077953a08b080c77e38901d8614
Stefan Aebischer <aebischer@pixtron.ch>
2017-09-13 17:18:28 +0100

    Update sync to 0.0.24

---
+++ app/migrations/v0.0.29.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+
+const logger = require('../lib/logger.js');
+const paths = require('../lib/paths.js');
+
+module.exports = function(previousVersion, currentVersion, done) {
+  logger.info('Running migraton to 0.0.29');
+
+  if(/^win/.test(process.platform) === false) {
+    logger.info('Migration to 0.0.29: not windows, no migration necessary');
+    return done(null, 'Not on windows, nothing to do');
+  }
+
+  const instancesFile = paths.getInstancesFile();
+  if(!fs.existsSync(instancesFile)) {
+    logger.info('Migration to 0.0.29: not instances, no migration necessary');
+    return done(null, 'No instances, nothing to do.');
+  }
+
+  try {
+    const instancesConfig = JSON.parse(fs.readFileSync(instancesFile, 'utf8'));
+    const instances = instancesConfig.instances;
+
+    if(instances) {
+      const instanceNames = Object.keys(instances);
+
+      instanceNames.forEach((instanceName) => {
+        const instanceDir = paths.getInstanceDir(instanceName);
+        if(fs.existsSync(instanceDir)) {
+          const pLastCursor = path.join(instanceDir, 'last-cursor');
+          if(fs.existsSync(pLastCursor)) {
+            fs.unlinkSync(pLastCursor);
+          }
+
+          const pNodesDb = path.join(instanceDir, 'db/nodes.db');
+          if(fs.existsSync(pNodesDb)) {
+            fs.unlinkSync(pNodesDb);
+          }
+
+          const pErrorQueueDb = path.join(instanceDir, 'db/api-error-queue.db');
+          if(fs.existsSync(pErrorQueueDb)) {
+            fs.unlinkSync(pErrorQueueDb);
+          }
+
+          const pTransferDb = path.join(instanceDir, 'db/transfer.db');
+          if(fs.existsSync(pTransferDb)) {
+            fs.unlinkSync(pTransferDb);
+          }
+        }
+      });
+    }
+
+    done(null, 'Removed cursor and sync db');
+  } catch(err) {
+    done(err);
+  }
+}
