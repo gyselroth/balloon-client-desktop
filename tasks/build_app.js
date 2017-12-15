0586e4823b609e788418607c71e5d33959d0a703
Raffael Sahli <public@raffaelsahli.com>
2017-12-15 16:33:12 +0100

    copy app env into user scope #64

--- tasks/build_app.js
+++ tasks/build_app.js
@@ -9,13 +9,14 @@ const utils = require('./utils');
 gulp.task('build', function () {
     var envName = utils.getEnvName();
     var srcPath = path.join(__dirname, '../config/', `env_${envName}.json`);
-    var destPath = path.join(__dirname, '../app/', 'env.json');
-
+    var destPath = path.join(__dirname, '../resources/', 'env.json');
+console.log(srcPath);
     if(fs.existsSync(srcPath)) {
+console.log(1);
       if(fs.existsSync(destPath)) {
         fs.truncateSync(destPath, 0);
       }
-
+console.log(destPath);
       fs.createReadStream(srcPath).pipe(fs.createWriteStream(destPath));
     } else if(envName !== 'production') {
       fs.writeFile(destPath, '{"name": "'+envName+'"}', function(err) {
