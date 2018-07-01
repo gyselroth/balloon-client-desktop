gyselroth/balloon-client-desktop

commit 9c16e78e19514f49199e1138947d4c3ebe008f48

Gracefully react on errors while creating error report
Resolves: #105
Resolves: #106

app/ui/feedback/controller.js
@@ -123,7 +123,10 @@ module.exports = function(env, clientConfig, sync) {
         async.parallel([
           (cb) => {
             createDirectorySnapshot((err, snapshot) => {
-              if(err) return cb(err);
+              if(err) {
+                logger.error('creating directory snapshot failed', {category: 'feedback', err});
+                return cb(err);
+              }
 
               archive.append(snapshot, {name: 'report/snapshot.json'});
 
@@ -141,8 +144,6 @@ module.exports = function(env, clientConfig, sync) {
 
           }
         ], (err, results) => {
-          if(err) return reject(err);
-
           archive.finalize();
         });
       }

commit 7a9458e6c57acdaa141e1e72dfbe9821394f3d37

Fixes regression introduced with 9c16e78
Resolves #107

app/ui/feedback/controller.js
@@ -178,7 +178,7 @@ module.exports = function(env, clientConfig) {
         archive.append(getMetaData(), {name: 'report/metadata.json'});
 
         async.parallel([
-          (cb) => {
+          async.reflect((cb) => {
             createDirectorySnapshot((err, snapshot) => {
               if(err) {
                 logger.error('creating directory snapshot failed', {category: 'feedback', err});
@@ -188,9 +188,9 @@ module.exports = function(env, clientConfig) {
               archive.append(snapshot, {name: 'report/snapshot.json'});
 
               cb(null);
-            })
-          },
-          (cb) => {
+            });
+          }),
+          async.reflect((cb) => {
             fsInfo(clientConfig.get('balloonDir'), (err, result) => {
               if(err) return cb(err);
 
@@ -198,9 +198,9 @@ module.exports = function(env, clientConfig) {
 
               return cb(null);
             });
-
-          }
+          })
         ], (err, results) => {
+          //err will be always `null` as async.reflect is used
           archive.finalize();
         });
       }