gyselroth/balloon-client-desktop

commit 2bc0d79fb9006ad69d3f205bf7315adff608fe23

Remove clientSecret from feedback reports
Resolves: #129

app/ui/feedback/controller.js
@@ -243,6 +243,20 @@ module.exports = function(env, clientConfig) {
       return value < 10 ? '0' + value : value;
     }
 
+    var envForReport = Object.assign({}, env);
+    if(envForReport.auth && envForReport.auth.oidc && envForReport.auth.oidc.isArray()) {
+      envForReport.auth.oidc = envForReport.auth.oidc.map(idpConfig => {
+        if(idpConfig.clientSecret) {
+          idpConfig.hasClientSecret = true;
+          delete idpConfig.clientSecret;
+        } else {
+          idpConfig.hasClientSecret = false;
+        }
+
+        return idpConfig;
+      });
+    }
+
     var metaData = {
       version: app.getVersion(),
       hasToken: clientConfig.get('accessToken') !== undefined,
@@ -253,7 +267,7 @@ module.exports = function(env, clientConfig) {
       },
       locale: app.getLocale(),
       config,
-      env: env,
+      env: envForReport,
       os: {
         arch: os.arch(),
         platform: os.platform(),

commit 03a01a1cf8b893afa2777513918c08dae358382f

Fix regression introduced in 2bc0d79

app/ui/feedback/controller.js
@@ -244,7 +244,7 @@ module.exports = function(env, clientConfig) {
     }
 
     var envForReport = Object.assign({}, env);
-    if(envForReport.auth && envForReport.auth.oidc && envForReport.auth.oidc.isArray()) {
+    if(envForReport.auth && envForReport.auth.oidc && Array.isArray(envForReport.auth.oidc)) {
       envForReport.auth.oidc = envForReport.auth.oidc.map(idpConfig => {
         if(idpConfig.clientSecret) {
           idpConfig.hasClientSecret = true;