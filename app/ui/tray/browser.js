gyselroth/balloon-client-desktop

commit 7be0cde35ab07fb4c0f75a4ca5a54f4e3dc56d2f

Support unlimited quota
Resolves: #109

app/ui/tray/browser.js
@@ -203,7 +203,21 @@ function showQuota() {
       return;
     }
 
-    var percent =  Math.round(data.used / data.hard_quota * 100, 0);
+    var percent, usage;
+
+    if(data.hard_quota <=0) {
+      percent = 0;
+      usage = i18n.__('tray.quota.unlimited');
+    } else {
+      percent =  Math.round(data.used / data.hard_quota * 100, 0);
+      usage = i18n.__('tray.quota.limited');
+    }
+
+    usage = [
+      getReadableFileSizeString(data.used),
+      getReadableFileSizeString(data.available)
+    ].reduce((p,c) => p.replace(/%s/,c), usage);
+
     var $used = $quota.find('.used');
     $used.width(percent+'%');
     $quota.show();
@@ -214,7 +228,7 @@ function showQuota() {
       $used.removeClass('quota-high');
     }
 
-    $quota.find('.quota-text').html('('+getReadableFileSizeString(data.available)+' left)');
+    $quota.find('.quota-text').html('('+usage+')');
   });
 }
 

commit b278bb4942a922ff0992bef5173ef295d89278ef

Fix quota display text
Fixes regression introduced with 7be0cde35ab07fb4c0f75a4ca5a54f4e3dc56d2f
Related to #109

app/ui/tray/browser.js
@@ -206,16 +206,11 @@ function showQuota() {
 
     if(data.hard_quota <=0) {
       percent = 0;
-      usage = i18n.__('tray.quota.unlimited');
+      usage = i18n.__('tray.quota.unlimited').replace(/%s/, getReadableFileSizeString(data.used));
     } else {
       percent =  Math.round(data.used / data.hard_quota * 100, 0);
-      usage = i18n.__('tray.quota.limited');
+      usage = i18n.__('tray.quota.limited').replace(/%s/, getReadableFileSizeString(data.available));
     }
-
-    usage = [
-      getReadableFileSizeString(data.used),
-      getReadableFileSizeString(data.available)
-    ].reduce((p,c) => p.replace(/%s/,c), usage);
 
     var $used = $quota.find('.used');
     $used.width(percent+'%');