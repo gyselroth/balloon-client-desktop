gyselroth/balloon-client-desktop

commit 77189d7515ef13d7e04cec6ddfc79f11afcfd3e0

Fix initial login state may differ
Resolves: #163

app/main.js
@@ -296,8 +296,31 @@ ipcMain.on('tray-online-state-changed', function(event, state) {
     if(sync && sync.isPaused() === false) sync.pause(true);
     tray.toggleState('offline', true);
   } else {
-    if(sync && sync.isPaused() === false) startSync(true);
     tray.toggleState('offline', false);
+
+    if(clientConfig.get('loggedin') === true) {
+      if(sync && sync.isPaused() === false) startSync(true);
+    } else if(startup.needsStartupWizzard() === false) {
+      logger.debug('Trying to verify user credentials', {category: 'main'});
+
+      // Pass rejected promise, as it should silently fail
+      auth.login(Promise.reject()).then(() => {
+        if(!sync) {
+          sync = SyncCtrl(env, tray);
+          sync.setMayStart(true);
+        }
+
+        clientConfig.updateTraySecret();
+        tray.toggleState('loggedout', false);
+
+        if(sync && sync.isPaused() === false) startSync(true);
+      }).catch((error) => {
+        logger.warning('User is not authenticated', {category: 'main'});
+
+        tray.emit('unlink-account-result', true);
+        tray.toggleState('loggedout', true);
+      });
+    }
   }
 });
 

app/ui/startup/controller.js
@@ -85,16 +85,7 @@ module.exports = function(env, clientConfig) {
   }
 
   function authenticate() {
-    if(!clientConfig.get('blnUrl')
-        ||
-        !clientConfig.get('apiUrl')
-        ||
-        !clientConfig.hadConfig()
-        ||
-        !clientConfig.isActiveInstance()
-        ||
-        instance.getInstance(clientConfig) === null
-    ) {
+    if(needsStartupWizzard()) {
       logger.debug('skip startup authentication, first time wizard needs to be executed first', {
           category: 'startup'
       });
@@ -112,6 +103,19 @@ module.exports = function(env, clientConfig) {
         reject(error);
       });
     });
+  }
+
+  function needsStartupWizzard() {
+    return (!clientConfig.get('blnUrl')
+        ||
+        !clientConfig.get('apiUrl')
+        ||
+        !clientConfig.hadConfig()
+        ||
+        !clientConfig.isActiveInstance()
+        ||
+        instance.getInstance(clientConfig) === null
+    );
   }
 
   function askCredentials() {
@@ -353,6 +357,7 @@ module.exports = function(env, clientConfig) {
   return {
     checkConfig,
     preSyncCheck,
-    showBalloonDir
+    showBalloonDir,
+    needsStartupWizzard
   }
 }

commit 3b0fa924b69780b14df2bcd5cbff1bae15efe2ac

Fix TypeError: startup is not a function
Fixes regression introduced with 77189d7

app/main.js
@@ -305,7 +305,7 @@ function bindOnlineStateChanged() {
         logger.debug('Trying to verify user credentials', {category: 'main'});
 
         // Pass rejected promise, as it should silently fail
-        auth.login(Promise.reject()).then(() => {
+        auth.login(() => Promise.reject()).then(() => {
           if(!sync) {
             sync = SyncCtrl(env, tray);
             sync.setMayStart(true);