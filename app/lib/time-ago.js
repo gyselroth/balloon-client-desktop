b05773ac91765312dad3e43ea652dc97caee1bb2
Stefan Aebischer <aebischer@pixtron.ch>
2020-01-10 11:56:03 -0500

    Localize time ago
    Resolves: #225

--- app/lib/time-ago.js
+++ app/lib/time-ago.js
@@ -2,6 +2,11 @@ const timeAgo = require('javascript-time-ago');
 const en = require('javascript-time-ago/locale/en');
 const de = require('javascript-time-ago/locale/de');
 
-timeAgo.addLocale(en);
+const i18n = require('./i18n.js');
 
-module.exports = new timeAgo('en');
+timeAgo.addLocale(en);
+timeAgo.addLocale(de);
+
+
+
+module.exports = new timeAgo(i18n.getCurrentLocale());
