gyselroth/balloon-client-desktop

commit a1c72b5f590e662cbafac776d3703ffdf693e97f

New tray behaviour for linux
Resolves: #207

app/ui/tray/controller.js
@@ -2,13 +2,14 @@ const path = require('path');
 const os = require('os');
 
 const electron = require('electron');
-const {app, BrowserWindow, ipcMain, nativeImage, Tray} = require('electron');
+const {app, BrowserWindow, ipcMain, Menu, nativeImage, Tray} = require('electron');
 const positioner = require('electron-traywindow-positioner');
 
 const url = require('url');
 const clientConfig = require('../../lib/config.js');
 
 const i18n = require('../../lib/i18n.js');
+const menuFactory = require('./menu-factory.js');
 
 const animationSpeed = 1000/24; //24 fps
 
@@ -16,6 +17,8 @@ const feedback = require('../feedback/controller.js');
 
 const logger = require('../../lib/logger.js');
 const contextMenuFactory = require('../../lib/context-menu-factory.js');
+
+const TRAY_CLICK_SHOW_WINDOW = process.platform !== 'linux';
 
 const stateIconNameMap = {
   default: 'default',
@@ -110,6 +113,9 @@ const currentStates = {
 
 const trayWindowHeight = 410;
 const trayWindowWidth = 500;
+
+let showLogin = true;
+let syncStatus = true;
 
 var tray;
 var animationTimeout = null;
@@ -224,9 +230,13 @@ module.exports = function(env, clientConfig) {
       tray = new Tray(icon);
       changeTrayIcon();
 
-      tray.on('click', function (event) {
-        toggle();
-      });
+      if (TRAY_CLICK_SHOW_WINDOW) {
+        tray.on('click', function (event) {
+          toggle();
+        });
+      }
+
+      updateTrayMenu();
 
       clientConfig.updateTraySecret();
     }
@@ -250,7 +260,7 @@ module.exports = function(env, clientConfig) {
     if(trayWindow) trayWindow.hide();
   }
 
-  function show() {
+  function show(menu='status') {
     logger.info('show tray window', {category: 'tray'});
 
     if(!trayWindow) trayWindow = createWindow();
@@ -258,7 +268,7 @@ module.exports = function(env, clientConfig) {
 
     //UPDATE ACCESS_TOKEN
 
-    trayWindow.webContents.send('update-window');
+    trayWindow.webContents.send('update-window', menu);
     positioner.position(trayWindow, tray.getBounds());
     trayWindow.setAlwaysOnTop(true);
     trayWindow.show();
@@ -301,8 +311,35 @@ module.exports = function(env, clientConfig) {
     return trayWindow;
   }
 
+  function loadMenu(menu) {
+    if(trayWindow.isVisible()) {
+      trayWindow.webContents.send('tray-load-menu', menu);
+    } else {
+      show(menu);
+    }
+  }
+
+  function updateTrayMenu() {
+    if (TRAY_CLICK_SHOW_WINDOW) return;
+
+    const menu = menuFactory(loadMenu, clientConfig, showLogin, syncStatus)
+
+    tray.setContextMenu(menu);
+  }
+
   function emit(key, message) {
     if(trayWindow) trayWindow.webContents.send(key, message);
+
+    switch(key) {
+      case 'unlink-account-result':
+        showLogin = message;
+        updateTrayMenu();
+      break;
+      case 'link-account-result':
+        showLogin = !message;
+        updateTrayMenu();
+      break;
+    }
   }
 
   ipcMain.on('tray-window-loaded', function(){
@@ -310,20 +347,28 @@ module.exports = function(env, clientConfig) {
   });
 
   function updateSecret() {
+    showLogin = (!clientConfig.get('loggedin') || !clientConfig.isActiveInstance())
     trayWindow.webContents.send('config', clientConfig.getSecret(), clientConfig.getSecretType());
+    updateTrayMenu()
   }
 
   function syncPaused() {
     trayWindow.webContents.send('sync-paused');
+    syncStatus = false;
+    updateTrayMenu();
   }
 
   function syncResumed() {
     trayWindow.webContents.send('sync-resumed');
+    syncStatus = true;
+    updateTrayMenu();
   }
 
   function syncStarted() {
     trayWindow.webContents.send('sync-started');
     toggleState('sync', true);
+    syncStatus = true;
+    updateTrayMenu();
   }
 
   function syncEnded() {

app/main.js
@@ -428,7 +428,7 @@ ipcMain.on('link-account', (event, id) => {
 
     tray.toggleState('loggedout', false);
     startSync(true);
-    event.sender.send('link-account-result', true);
+    tray.emit('link-account-result', true);
   }).catch((err) => {
     if(err.code !== 'E_BLN_OAUTH_WINDOW_OPEN') {
       logger.error('login not successfull', {
@@ -441,7 +441,7 @@ ipcMain.on('link-account', (event, id) => {
       });
     }
 
-    event.sender.send('link-account-result', false);
+    tray.emit('link-account-result', false);
   });
 });
 

app/ui/tray/browser.js
@@ -9,6 +9,7 @@ const {fullSyncFactory} = require('@gyselroth/balloon-node-sync');
 
 const i18n = require('../../lib/i18n.js');
 const instance = require('../../lib/instance.js');
+const menuFactory = require('./menu-factory.js');
 const clientConfig = require('../../lib/config.js');
 const globalConfig = require('../../lib/global-config.js');
 const appState = require('../../lib/state.js');
@@ -60,70 +61,7 @@ $(window).blur(function(){
 });
 
 function buildMenu() {
-  var label;
-  const {Menu, MenuItem} = remote
-  const menu = new Menu()
-
-  if(showLogin === false && clientConfig.get('username')) {
-    menu.append(new MenuItem({label: clientConfig.get('username'), enabled: false}))
-  }
-
-  if(showLogin === true) {
-    label = i18n.__('tray.menu.link');
-    menu.append(new MenuItem({label: label, click: function(){
-      ipcRenderer.send('link-account');
-      ipcRenderer.send('tray-hide');
-    }}))
-  } else {
-    label = i18n.__('tray.menu.unlink');
-    menu.append(new MenuItem({label: label, click: function(){
-      ipcRenderer.send('unlink-account');
-      ipcRenderer.send('tray-hide');
-    }}))
-  }
-
-  menu.append(new MenuItem({type: 'separator', enabled: false}))
-
-  if(clientConfig.get('loggedin') === true) {
-    if(syncStatus === true) {
-      label = i18n.__('tray.menu.pauseSync');
-    } else {
-      label = i18n.__('tray.menu.continueSync');
-    }
-    menu.append(new MenuItem({label: label, click:function(){
-      ipcRenderer.send('sync-toggle-pause');
-      ipcRenderer.send('tray-hide');
-    }}))
-
-    menu.append(new MenuItem({type: 'separator', enabled: false}))
-  }
-
-  label = i18n.__('tray.menu.status');
-  menu.append(new MenuItem({label: label, click: function(){
-    loadMenu('status');
-  }}))
-
-  label = i18n.__('tray.menu.settings');
-  menu.append(new MenuItem({label: label, click: function(){
-    loadMenu('settings');
-  }}))
-
-  label = i18n.__('tray.menu.feedback');
-  menu.append(new MenuItem({label: label, click: function(){
-    loadMenu('feedback');
-  }}))
-
-  label = i18n.__('tray.menu.about');
-  menu.append(new MenuItem({label: label, click: function(){
-    loadMenu('about');
-  }}))
-
-  menu.append(new MenuItem({type: 'separator', enabled: false}))
-
-  label = i18n.__('tray.menu.close');
-  menu.append(new MenuItem({label: label, click: function(){
-    ipcRenderer.send('quit');
-  }}))
+  const menu = menuFactory(loadMenu, clientConfig, showLogin, syncStatus);
 
   menu.popup(remote.getCurrentWindow());
 }
@@ -132,7 +70,9 @@ $('document').ready(function() {
   $('body').addClass(process.platform);
   compileTemplate();
 
-  ipcRenderer.on('update-window', updateWindow);
+  ipcRenderer.on('update-window', (event, menu) => {
+    updateWindow(menu)
+  });
 
   $('#item-installupdate').bind('click', function() {
     ipcRenderer.send('tray-hide');
@@ -173,6 +113,10 @@ ipcRenderer.on('sync-paused' , function() {
   syncStatus = false;
 });
 
+ipcRenderer.on('tray-load-menu' , function(event, menu) {
+  loadMenu(menu);
+});
+
 ipcRenderer.send('tray-window-loaded');
 ipcRenderer.on('config', function(event, secret, secretType) {
   logger.info('got config', {category: 'tray-browser', secretType});
@@ -209,13 +153,13 @@ function toggleInstallUpdate() {
   }
 }
 
-function updateWindow() {
+function updateWindow(menu='status') {
   logger.info('updateWindow', {category: 'tray-browser'});
 
   //TODO pixtron - do we still need this?
   $('#tray-main').html('');
   toggleInstallUpdate();
-  loadMenu('status');
+  loadMenu(menu);
 }
 
 function getOnLineState(callback) {

app/ui/tray/menu-factory.js
@@ -1,0 +1,90 @@
+
+const i18n = require('../../lib/i18n.js');
+
+let Menu, MenuItem, emit;
+if(!process || process.type === 'renderer') {
+  const {ipcRenderer, remote} = require('electron');
+  Menu = remote.Menu;
+  MenuItem = remote.MenuItem;
+  emit = function() {
+    ipcRenderer.send(...arguments);
+  }
+
+} else {
+  const electron = require('electron');
+  const ipcMain = electron.ipcMain;
+  Menu = electron.Menu;
+  MenuItem = electron.MenuItem;
+
+  emit = function() {
+    ipcMain.emit(...arguments);
+  }
+}
+
+module.exports = function menuFactory(loadMenu, clientConfig, showLogin=true, syncStatus=true) {
+  var label;
+  const menu = new Menu()
+
+  if(showLogin === false && clientConfig.get('username')) {
+    menu.append(new MenuItem({label: clientConfig.get('username'), enabled: false}))
+  }
+
+  if(showLogin === true) {
+    label = i18n.__('tray.menu.link');
+    menu.append(new MenuItem({label: label, click: function(){
+      emit('link-account');
+      emit('tray-hide');
+    }}))
+  } else {
+    label = i18n.__('tray.menu.unlink');
+    menu.append(new MenuItem({label: label, click: function(){
+      emit('unlink-account');
+      emit('tray-hide');
+    }}))
+  }
+
+  menu.append(new MenuItem({type: 'separator', enabled: false}))
+
+  if(clientConfig.get('loggedin') === true) {
+    if(syncStatus === true) {
+      label = i18n.__('tray.menu.pauseSync');
+    } else {
+      label = i18n.__('tray.menu.continueSync');
+    }
+    menu.append(new MenuItem({label: label, click:function(){
+      emit('sync-toggle-pause');
+      emit('tray-hide');
+    }}))
+
+    menu.append(new MenuItem({type: 'separator', enabled: false}))
+  }
+
+  label = i18n.__('tray.menu.status');
+  menu.append(new MenuItem({label: label, click: function(){
+    loadMenu('status');
+  }}))
+
+  label = i18n.__('tray.menu.settings');
+  menu.append(new MenuItem({label: label, click: function(){
+    loadMenu('settings');
+  }}))
+
+  label = i18n.__('tray.menu.feedback');
+  menu.append(new MenuItem({label: label, click: function(){
+    loadMenu('feedback');
+  }}))
+
+  label = i18n.__('tray.menu.about');
+  menu.append(new MenuItem({label: label, click: function(){
+    loadMenu('about');
+  }}))
+
+  menu.append(new MenuItem({type: 'separator', enabled: false}))
+
+  label = i18n.__('tray.menu.close');
+  menu.append(new MenuItem({label: label, click: function(){
+    emit('quit');
+  }}))
+
+  return menu;
+}

commit 84b6bbe25e2773ddc5f6fc23efb4e55df5430d4a

Fix TypeError: Cannot read property 'setContextMenu' of undefined
Fixes regression introduced with a1c72b5
Related to: #207

app/ui/tray/controller.js
@@ -320,7 +320,7 @@ module.exports = function(env, clientConfig) {
   }
 
   function updateTrayMenu() {
-    if (TRAY_CLICK_SHOW_WINDOW) return;
+    if (TRAY_CLICK_SHOW_WINDOW || !tray) return;
 
     const menu = menuFactory(loadMenu, clientConfig, showLogin, syncStatus)
 